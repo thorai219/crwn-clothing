a7cbf1c129549aac27d62fab684f5945d55bfaa9
Terry Lee <slsk0219@gmail.com>
2021-02-15 12:12:09 -0800

    update shop-page, collection-item/preview

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import "./App.css";
 import Homepage from "./pages/homepage/homepage.component.jsx";
+import ShopPage from "./pages/shop/shop.component";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import CollectionPreview from "../../components/preview-collection/collection-preview.component";
+import SHOP_DATA from "./shop.data";
+
+class ShopPage extends React.Component {
+  constructor(props) {
+    super();
+
+    this.state = {
+      collections: SHOP_DATA,
+    };
+  }
+
+  render() {
+    const { collections } = this.state;
+    return (
+      <div className="shop-page">
+        {collections.map(({ id, ...otherCollectionsProps }) => (
+          <CollectionPreview key={id} {...otherCollectionsProps} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default ShopPage;
