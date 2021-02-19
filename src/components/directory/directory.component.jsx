2f90214ecb53c0cd2f5514976e25382f31c6d5c3
Terry Lee <slsk0219@gmail.com>
2021-02-18 17:05:17 -0800

    added directory sections into redux

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,57 +1,24 @@
 import React, { Component } from "react";
+
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+import { selectDirectorySection } from "../../redux/directory/directory.selector";
+
 import MenuItem from "../menu-item/menu-item.component";
 import "./directory.styles.scss";
 
-class directory extends Component {
-  constructor() {
-    super();
+const directory = ({ sections }) => {
+  return (
+    <div className="directory-menu">
+      {sections.map(({ id, ...otherSectionProps }) => (
+        <MenuItem key={id} {...otherSectionProps} />
+      ))}
+    </div>
+  );
+};
 
-    this.state = {
-      sections: [
-        {
-          title: "hats",
-          imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-          id: 1,
-          linkUrl: "shop/hats",
-        },
-        {
-          title: "jackets",
-          imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-          id: 2,
-          linkUrl: "shop/jackets",
-        },
-        {
-          title: "sneakers",
-          imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-          id: 3,
-          linkUrl: "shop/sneakers",
-        },
-        {
-          title: "womens",
-          imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-          size: "large",
-          id: 4,
-          linkUrl: "shop/womens",
-        },
-        {
-          title: "mens",
-          imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-          size: "large",
-          id: 5,
-          linkUrl: "shop/mens",
-        },
-      ],
-    };
-  }
-  render() {
-    return (
-      <div className="directory-menu">
-        {this.state.sections.map(({ id, ...otherSectionProps }) => (
-          <MenuItem key={id} {...otherSectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
+const mapStateToProps = createStructuredSelector({
+  sections: selectDirectorySection,
+});
 
-export default directory;
+export default connect(mapStateToProps)(directory);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,13 @@
-import {createStore, applyMiddleware} from 'redux'
-import logger from 'redux-logger'
+import { createStore, applyMiddleware } from "redux";
+import logger from "redux-logger";
+import { persistStore } from "redux-persist";
 
-import rootReducer from './rootReducer'
+import rootReducer from "./rootReducer";
 
-const middlewares = [logger]
+const middlewares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares))
+const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export default store
+const persistor = persistStore(store);
+
+export { store, persistor };
